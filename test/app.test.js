2aeb99708ce66a3a3ba3af26ac71faed5c18a7c2
hoani <hoani.bryson@gmail.com>
2019-11-20 09:19:43 +1300

    Add timing command line arguments

--- test/app.test.js
+++ test/app.test.js
@@ -11,30 +11,61 @@ describe('Command Line Interface - IP Address', function() {
   });
   
   it('New Address', function() {
-    process.argv = 'node app.test.js -a 0.0.0.0 1234'.split(' ');
+    process.argv = 'node app.test.js --address 0.0.0.0 1234'.split(' ');
     result = app.parseCliArguments();
     assert.equal(result.address, "ws://0.0.0.0:1234");
   });
   
   it('Change ip address only', function() {
-    process.argv = 'node app.test.js -a 0.0.0.0'.split(' ');
+    process.argv = 'node app.test.js --address 0.0.0.0'.split(' ');
     result = app.parseCliArguments();
     assert.equal(result.address, "ws://0.0.0.0:9944");
   });
   
   it('IP address can be a string', function() {
-    process.argv = 'node app.test.js -a node 1234'.split(' ');
+    process.argv = 'node app.test.js --address node 1234'.split(' ');
     result = app.parseCliArguments();
     assert.equal(result.address, "ws://node:1234");
   });
   
   it('Port must be a number', function() {
-    process.argv = 'node app.test.js -a 0.0.0.0 five'.split(' ');
+    process.argv = 'node app.test.js --address 0.0.0.0 five'.split(' ');
     result = app.parseCliArguments();
     assert.equal(result.address, "ws://0.0.0.0:9944");
   });
+});
+
+describe('Command Line Interface - Transaction Params', function() {
+  it('Default Timing', function() {
+    process.argv = ['node','app.test.js'];
+    result = app.parseCliArguments();
+    assert.equal(result.transaction.timeout_ms, 5000);
+    assert.equal(result.transaction.period_ms, 5000);
+  });
   
+  it('Set timeout', function() {
+    process.argv = 'node app.test.js --timeout 1234'.split(' ');
+    result = app.parseCliArguments();
+    assert.equal(result.transaction.timeout_ms, 1234);
+  });
   
+  it('Set period', function() {
+    process.argv = 'node app.test.js --period 1234'.split(' ');
+    result = app.parseCliArguments();
+    assert.equal(result.transaction.period_ms, 1234);
+  });
+  
+  it('Set bad timeout', function() {
+    process.argv = 'node app.test.js --timeout five'.split(' ');
+    result = app.parseCliArguments();
+    assert.equal(result.transaction.timeout_ms, 5000);
+  });
+  
+  it('Set bad period', function() {
+    process.argv = 'node app.test.js --period five'.split(' ');
+    result = app.parseCliArguments();
+    assert.equal(result.transaction.period_ms, 5000);
+  });
 });
 
 describe('Send_and_receive_keypair_selection', function() {
