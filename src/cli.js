4e8109451e660ae0214cc2e734d06c1bed33ef1f
Eric <e.lim0322@gmail.com>
2019-12-09 13:13:12 +1300

    submit transactions in round-robin, add log-level to cli and associated tests

--- src/cli.js
+++ src/cli.js
@@ -11,11 +11,12 @@ function forceInt(value, default_value) {
 
 function parseCliArguments() {
   const default_ip_address = '127.0.0.1';
-  const default_port = 9944;
+  const default_port = '9944';
   const default_timeout_ms = 5000;
   const default_period_ms = 5000;
   const default_block_delta = 10000;
   const default_startup_delay_ms = 0;
+  const default_log_level = 'info';
 
 
   let parser = ArgParse.ArgumentParser()
@@ -23,8 +24,8 @@ function parseCliArguments() {
     ['--address'],
     {
       help: 'IP address of the node for sending transactions',
-      defaultValue: [default_ip_address, default_port],
-      metavar: ['ip-addr', 'port-num'],
+      defaultValue: [default_ip_address],
+      metavar: ['ip-addr'],
       nargs: '+',
       dest: 'address'
     }
@@ -80,25 +81,16 @@ function parseCliArguments() {
     }
   );
   parser.addArgument(
-    ['--verbose'],
+    ['--log-level'],
     {
-      help: 'Run in verbose mode',
-      defaultValue: false,
-      action: 'storeTrue',
-      nargs: '0',
-      dest: 'verbose'
+      help: 'Set log-level',
+      defaultValue: default_log_level,
+      nargs: '1',
+      dest: 'log_level'
     }
   );
 
   let args = parser.parseArgs()
-
-  // Fill in any blanks for the address config
-  if (args.address.length == 1) {
-    args.address.push(default_port);
-  }
-  else if (args.address.length >= 2) {
-    args.address[1] = forceInt(args.address[1], default_port);
-  }
 
   // Force all integers to be integers
   args.timeout_ms = forceInt(args.timeout_ms, default_timeout_ms);
@@ -106,9 +98,19 @@ function parseCliArguments() {
   args.block_delta = forceInt(args.block_delta, default_block_delta);
   args.startup_delay_ms = forceInt(args.startup_delay_ms, default_startup_delay_ms);
 
+  // Format addresses and port correctly
+  let addresses = [];
+  for (let i = 0; i < args.address.length; i++) {
+    // Add default port if not specified
+    if (args.address[i].split(':').length == 1) {
+      args.address[i] = [args.address[i], default_port].join(":")
+    }
+    addresses.push(`ws://${args.address[i]}`);
+  }
+
   // Return a settings object
   let settings = {
-    address: `ws://${args.address[0]}:${args.address[1]}`,
+    address: addresses,
     startup_delay_ms: args.startup_delay_ms,
     transaction: {
       timeout_ms: args.timeout_ms,
@@ -118,7 +120,7 @@ function parseCliArguments() {
       block_delta: args.block_delta
     },
     fund: args.fund,
-    verbose: args.verbose
+    log_level: Array.isArray(args.log_level) ? args.log_level[0] : args.log_level
   }
   return settings;
 }
@@ -133,4 +135,3 @@ if (require.main === module) {
     parseCliArguments: parseCliArguments
   }
 }
-
