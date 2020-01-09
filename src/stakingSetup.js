f4e8e7a2eee50cb93b6eec4cdbb83a8061075da5
Triet <triet.nguyen267@gmail.com>
2020-01-09 13:52:26 +1300

    Add semaphore for manage race-conditions and refactor code

--- src/stakingSetup.js
+++ src/stakingSetup.js
@@ -21,87 +21,50 @@ const controllerAddrs = [
 async function createStashAccounts(api) {
     stashAccount = []
     stashNames = ["BOB_STASH", "CHARLIE_STASH", "EVE_STASH"];
-    acliceKeyring = defaulKeyrings.alice;
+    aliceKeyring = defaulKeyrings.alice;
 
     for (i = 0; i < stashNames.length; i++) {
         name = stashNames[i];
         stashAccount.push(keyring.addFromUri(name, { name: name }));
     }
 
-    for (account of stashAccount) {
-        log.info("SETTING UP FOR CENNZ -------------- \n Accout raw seed: " + account.meta.name)
-        log.info("Transfering cennz to " + account.meta.name)            
-            const cennz = await api.tx.genericAsset.transfer(16000, account.address, "100_000_000_000_000")
-            .signAndSend(acliceKeyring, (result) => {
-                log.info(`Current status is ${result.status}`);
-
-                if (result.isCompleted) {
-                    if (result.isFinalized) {
-                        hash = result.status.asFinalized;
-                    }
-                    else { // error
-                        log.info("Transaction Rejected");
-                        throw [APP_FAIL_TRANSACTION_REJECTED, `Transaction rejected ${result.status} for ${account.meta.name}`];
-                    }
-                    cennz();
-                }
-            })
-            .catch(err => transaction_error = err);
-            await sleep();
-            
-    }
-
-    for (account of stashAccount) {
-        log.info("SETTING UP FOR CENTRAPAY -------------- \n Accout raw seed: " + account.meta.name)
-        log.info(`Transfering centrapay to ${account.meta.name} and address ${account.address}`)
-        const unsub = await api.tx.genericAsset.transfer(16001, account.address, "100_000_000_000_000")
-            .signAndSend(acliceKeyring, (result) => {
-                log.info(`Current status is ${result.status}`);
-
-                if (result.isCompleted) {
-                    if (result.isFinalized) {
-                        hash = result.status.asFinalized;
-                    }
-                    else { // error
-                        log.info("Transaction Rejected");
-                        throw [APP_FAIL_TRANSACTION_REJECTED, `Transaction rejected ${result.status}`];
-                    }
-                    unsub();
-                }
-            })
-            .catch(err => transaction_error = err);
-            await sleep();
-    }
-
+    await topUpStashAccount(api, 16000, stashAccount);
+    await topUpStashAccount(api, 16001, stashAccount);
     await bondStashAccounts(api, stashAccount);
     await setSessionKey(api);
     await setNodesValidating(api);
 }
 
-function sleep(){
+function sleep(ms = DEFAULT_SLEEP){
     return new Promise(resolve=>{
-        setTimeout(resolve,DEFAULT_SLEEP)
+        setTimeout(resolve,ms)
     })
 }
 
 async function bondStashAccounts(api, stashes){
     
     for(i=0; i< controllerAddrs.length; i++){
-       let transfer = api.tx.staking.bond(controllerAddrs[i], "5_000_000", 0);
+        let lock = true;
+        let transfer = api.tx.staking.bond(controllerAddrs[i], "5_000_000", 0);
         // Sign and Send the transaction
-        transfer.signAndSend(stashes[i], ({ events = [], status, type }) => {
-            if (type === 'Finalised') {
-                console.log('Successful transfer ' + ' with hash ' + status.asFinalised.toHex());
-              } else {
-                console.log('Status of transfer: ' + type);
-              }
-              events.forEach(({ phase, event: { data, method, section } }) => {
-                console.log(phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString());
-              });
+        transfer.signAndSend(stashes[i], (result) => {
+            if (result.isCompleted) {
+                if (result.isFinalized) {
+                    hash = result.status.asFinalized;
+                    log.info(`bondStashAccounts Status is ${result.isFinalized} with hash ${hash}`);
+                }
+                else { // error
+                    log.error(`Account ${stashes[i]} failed to bond with Controller ${controllerAddrs[i]}`);
+                    throw [`Transaction rejected ${result.status} for ${stashes[i]}`];
+                }
+                lock = false
+            }
         })
         .catch(err => transaction_error = err);
 
-        await sleep();
+        while (lock){
+            await sleep(200);
+        }
     }
 }
 
@@ -114,15 +77,32 @@ async function setSessionKey(api){
     ];
 
     for(i=0; i<sessionKeys.length; i++){
+        log.info(`Setting session key ${controllersKeyring[i].meta.name}`)
+        let lock = true;
         try{
             const transfer = await api.tx.session.setKey(sessionKeys[i]);
-            transfer.signAndSend(controllersKeyring[i])
+            transfer.signAndSend(controllersKeyring[i], (result) => {
+                
+                if(result.isCompleted){
+                    if (result.isFinalized) {
+                        hash = result.status.asFinalized;
+                        log.info(`Setting session key Status is ${result.isFinalized} with hash ${hash}`);
+                    }
+                    else { // error
+                        log.error("Set session key failed");
+                        throw [`Failed to set session key ${result.status} for ${controllersKeyring[i].meta.name}`];
+                    }
+                    lock = false;
+                }
+            })
             
         }catch(err){
             console.log(err)
         };
 
-        await sleep();
+        while (lock){
+            await sleep(200)
+        }
     }
 }
 
@@ -132,19 +112,54 @@ async function setNodesValidating(api){
     for(controller of controllersKeyring){
         // Sign and Send the transaction
         const transfer = api.tx.staking.validate(preferences);
-        transfer.signAndSend(controller, ({ events = [], status, type }) => {
-            if (type === 'Finalised') {
-                console.log('Successful transfer ' + ' with hash ' + status.asFinalised.toHex());
-            } else {
-                console.log('Status of transfer: ' + type);
+        log.info(`Setting up staking validator node ${controller.meta.name}`)
+        transfer.signAndSend(controller, (result) => {
+            if(result.isCompleted){
+                if (result.isFinalized) {
+                    hash = result.status.asFinalized;
+                    log.info(`setNodesValidating Status is ${result.isFinalized} with hash ${hash}`);
+                }
+                else { // error
+                    log.error("Set staking validator node failed");
+                    throw [`Failed to set staking account as validator ${result.status} for ${controller.meta.name}`];
+                }
+                lock = false;
             }
-            events.forEach(({ phase, event: { data, method, section } }) => {
-                console.log(phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString());
-            })           
         })
         .catch(err => transaction_error = err);
 
-        await sleep();
+        while (lock){
+            await sleep(200);
+        }
+    }
+}
+
+async function topUpStashAccount(api, type, stashAccounts){   
+    lock = false; 
+    for (account of stashAccounts) {
+        lock = true;
+        log.info(`TOP UP stash balance with ${type} -------------- \n ${account.meta.name}`)
+        log.info(`Transfering to ${account.meta.name}`)            
+            const unsub = await api.tx.genericAsset.transfer(type, account.address, "100_000_000_000_000")
+            .signAndSend(aliceKeyring, (result) => {
+                if (result.isCompleted) {
+                    if (result.isFinalized) {
+                        hash = result.status.asFinalized;
+                        log.info(`topUpStashAccount Status is ${result.isFinalized} with hash ${hash}`);
+                    }
+                    else { // error
+                        log.error("Transaction Rejected");
+                        throw [`Transaction rejected ${result.status} for ${account.meta.name}`];
+                    }
+                    unsub();
+                    lock = false;
+                }
+            })
+            .catch(err => transaction_error = err);    
+
+            while(lock){
+                await sleep(200);
+        }           
     }
 }
 
